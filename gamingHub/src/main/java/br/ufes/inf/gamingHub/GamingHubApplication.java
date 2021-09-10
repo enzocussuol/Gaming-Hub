a754411f77cefa0ce10856e21fe9a7ee10f31a52
enzocussuol <enzo@allsystem.com.br>
2021-09-10 18:46:03 -0300

    Primeiro commit

---
+++ gamingHub/src/main/java/br/ufes/inf/gamingHub/GamingHubApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.inf.gamingHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingHubApplication.class, args);
+	}
+
+}
---
+++ gamingHub/src/test/java/br/ufes/inf/gamingHub/GamingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.inf.gamingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
