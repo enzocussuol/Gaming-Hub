da094900abed85c2e99e3d3870c9359d69d89aaf
enzocussuol <enzo@allsystem.com.br>
2021-09-16 12:03:42 -0300

    Requisicao http feita na api appdetails do jogo half-life 2

--- gamingHub/src/main/java/br/ufes/inf/gamingHub/GamingHubApplication.java
+++ gamingHub/src/main/java/br/ufes/inf/gamingHub/GamingHubApplication.java
@@ -1,5 +1,11 @@
 package br.ufes.inf.gamingHub;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +14,25 @@ public class GamingHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamingHubApplication.class, args);
+		
+		String respostaHttp = null;
+		
+		try {
+			String url = "https://store.steampowered.com/api/appdetails?appids=220";
+			CloseableHttpClient clienteHttp = HttpClients.createDefault();
+			HttpGet getHttp = new HttpGet(url);
+			
+			System.out.println("Fazendo download...");
+			
+			CloseableHttpResponse resposta = clienteHttp.execute(getHttp);
+			HttpEntity entidade = resposta.getEntity();
+			
+			System.out.println("Download terminado!");
+			
+			respostaHttp = EntityUtils.toString(entidade);
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-
 }
